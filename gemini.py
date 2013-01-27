7d8de4d5ce10e3258ea8257fe0042998506fbf19
Adam Petrone <adampetrone83@gmail.com>
2013-01-27 01:33:41 -0500

    Tweaking iphone support on main pegasus file.

--- gemini.py
+++ gemini.py
@@ -1,4 +1,4 @@
-class gemini(Builder): 
+class gemini(Builder):
 	def setup(self, platform, builder):
 		self.build_name = 'gemini'
 		project = Project( name=self.build_name )
@@ -7,15 +7,13 @@ class gemini(Builder):
 		builder.libs = []
 		builder.root = "build"
 
-		if builder.platform is MACOSX:
-			self.builder_type = Builder.Bundle
-		else:
-			self.builder_type = Builder.Binary
+		self.builder_type = DefaultDict( Builder.Bundle )
+		self.builder_type[ MACOSX ] = Builder.Bundle
 
 	def config(self, builder, driver, project, args):
 		bindir = "latest/bin/%s/%s" % (args['build_architecture'], args['configuration'])
 
-		builder.setOutput( path=bindir, name=self.build_name, type=self.builder_type )
+		builder.setOutput( path=bindir, name=self.build_name, type=self.builder_type[ args["platform"] ] )
 
 		driver.config = (args['configuration'].lower() + Premake4.archmap[ args['platform'] ][ args['build_architecture'] ])
 		driver.makefile = "%s.make" % (self.build_name)
@@ -36,8 +34,10 @@ class gemini(Builder):
 			arch_list.append( 'Native' )
 
 		premake = Premake4( action=builder.premake_action, file="premake4.lua", platform_list=",".join( arch_list ) )
-		#premake.ios = True
+		if builder.platform == IPHONEOS:
+			premake.ios = True
 		premake.run()
+
 
 	def prebuild(self, driver, args):
 		args['DEBUG_INFORMATION_FORMAT'] = "dwarf-with-dsym"
