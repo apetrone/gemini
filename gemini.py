17fd5c4ab505b704bead7aa87a1b2c07839a5c88
Adam Petrone <adampetrone83@gmail.com>
2013-01-27 11:17:10 -0500

    Utilize refactored pegasus setup with *args and *kwargs for gemini.py

--- gemini.py
+++ gemini.py
@@ -1,5 +1,7 @@
 class gemini(Builder):
-	def setup(self, platform, builder):
+	def setup(self, *args, **kwargs):
+		builder = kwargs.get( "builder", None )
+
 		self.build_name = 'gemini'
 		project = Project( name=self.build_name )
 		builder.addProject( project )
@@ -7,15 +9,23 @@ class gemini(Builder):
 		builder.libs = []
 		builder.root = "build"
 
-		self.builder_type = DefaultDict( Builder.Bundle )
+		self.builder_type = DefaultDict( Builder.Binary )
 		self.builder_type[ MACOSX ] = Builder.Bundle
+		self.builder_type[ IPHONEOS ] = Builder.Bundle
 
-	def config(self, builder, driver, project, args):
-		bindir = "latest/bin/%s/%s" % (args['build_architecture'], args['configuration'])
+	def config(self, *args, **kwargs):
+		driver = kwargs.get( "driver", None )
+		builder = kwargs.get( "builder", None )
+		host_platform = kwargs.get( "host_platform", None )
+		target_platform = kwargs.get( "target_platform", host_platform )
+		project = kwargs.get( "project", None )
+		params = kwargs.get( "args", None )
 
-		builder.setOutput( path=bindir, name=self.build_name, type=self.builder_type[ args["platform"] ] )
+		bindir = "latest/bin/%s/%s" % (params['build_architecture'], params['configuration'])
 
-		driver.config = (args['configuration'].lower() + Premake4.archmap[ args['platform'] ][ args['build_architecture'] ])
+		builder.setOutput( path=bindir, name=self.build_name, type=self.builder_type[ target_platform ] )
+
+		driver.config = (params['configuration'].lower() + Premake4.archmap[ params['platform'] ][ params['build_architecture'] ])
 		driver.makefile = "%s.make" % (self.build_name)
 
 	@staticmethod
@@ -27,18 +37,24 @@ class gemini(Builder):
 		d['depends'] = [ ]
 		return d
 
-	def generate(self, builder):
+	def generate(self, *args, **kwargs):
+		builder = kwargs.get( "builder", None )
+		host_platform = kwargs.get( "host_platform", None )
+		target_platform = kwargs.get( "target_platform", host_platform )
+
 		arch_list = builder.arch_list[:]
 		if 'native' in arch_list:
 			arch_list.remove( 'native' )
 			arch_list.append( 'Native' )
 
 		premake = Premake4( action=builder.premake_action, file="premake4.lua", platform_list=",".join( arch_list ) )
-		if builder.platform == IPHONEOS:
+		if target_platform == IPHONEOS:
 			premake.ios = True
 		premake.run()
 
+	def prebuild(self, *args, **kwargs):
+		driver = kwargs.get( "driver", None )
+		params = kwargs.get( "args", None )
 
-	def prebuild(self, driver, args):
-		args['DEBUG_INFORMATION_FORMAT'] = "dwarf-with-dsym"
-		args['LD_DYLIB_INSTALL_NAME'] = "@executable_path/./"
+		params['DEBUG_INFORMATION_FORMAT'] = "dwarf-with-dsym"
+		params['LD_DYLIB_INSTALL_NAME'] = "@executable_path/./"
