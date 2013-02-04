336660278480b2b80bd388bc1894e36cc6bceea9
Adam Petrone <adampetrone83@gmail.com>
2013-02-03 23:59:28 -0500

    Show a magenta screen for now instead of the default.

--- build/src/ios/ViewController.mm
+++ build/src/ios/ViewController.mm
@@ -140,6 +140,8 @@
 - (void)update
 {
 	//NSLog( @"ViewController.m - update" );
+	glClearColor( 1.0f, 0.0f, 1.0f, 1.0f );
+	glClear( GL_COLOR_BUFFER_BIT );
 }
 
 - (void)glkView:(GLKView *)view drawInRect:(CGRect)rect
