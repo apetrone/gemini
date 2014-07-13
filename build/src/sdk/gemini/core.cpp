2244b8351fa8633f7ed861c5c6e1fcc8c2f7ef90
Adam Petrone <adampetrone83@gmail.com>
2014-07-13 01:45:55 -0400

    hooked up logging again (oops)

--- build/src/sdk/gemini/core.cpp
+++ build/src/sdk/gemini/core.cpp
@@ -30,6 +30,9 @@
 
 #include <gemini/util/stackstring.h>
 
+#include <slim/xlog.h>
+#include <slim/xtime.h>
+
 namespace core
 {
 	namespace _internal
@@ -45,9 +48,112 @@ namespace core
 			filesystem::content_directory( &root[0], root.max_size() );
 		}
 				
+		static xlog_t _system_log;
+		// log handler function declarations
+		void file_logger_message( xlog_handler_t * handler, const char * message, const char * filename, const char * function, int line, int type );
+		int file_logger_open( xlog_handler_t * handler );
+		void file_logger_close( xlog_handler_t * handler );
+		
+		void stdout_message( xlog_handler_t * handler, const char * message, const char * filename, const char * function, int line, int type );
+		int stdout_open( xlog_handler_t * handler );
+		void stdout_close( xlog_handler_t * handler );
+		
+#if _WIN32
+		void vs_message( xlog_handler_t * handler, const char * message, const char * filename, const char * function, int line, int type );
+		int vs_open( xlog_handler_t * handler );
+		void vs_close( xlog_handler_t * handler );
+#endif
+		
+#if __ANDROID__
+		void log_android_message( xlog_handler_t * handler, const char * message, const char * filename, const char * function, int line, int type );
+		int log_android_open( xlog_handler_t * handler );
+		void log_android_close( xlog_handler_t * handler );
+#endif
+				
 		core::Error open_log_handlers()
 		{
 			core::Error error( 0 );
+			
+			int total_log_handlers = 0;
+			
+			// setup system log
+			xlog_init( &_system_log );
+			
+			xlog_set_default_log( &_system_log );
+			
+#if _WIN32
+			xlog_handler_t msvc_logger;
+			memset( &msvc_logger, 0, sizeof(xlog_handler_t) );
+			msvc_logger.close = vs_close;
+			msvc_logger.open = vs_open;
+			msvc_logger.message = vs_message;
+			msvc_logger.userdata = 0;
+			
+			++total_log_handlers;
+			xlog_add_handler( &_system_log, &msvc_logger );
+#endif
+			
+#ifndef __ANDROID__
+			xlog_handler_t stdout_logger;
+			memset( &stdout_logger, 0, sizeof(xlog_handler_t) );
+			stdout_logger.message = stdout_message;
+			stdout_logger.open = stdout_open;
+			stdout_logger.close = stdout_close;
+			
+			++total_log_handlers;
+			xlog_add_handler( &_system_log, &stdout_logger );
+#endif
+			
+#if !PLATFORM_IS_MOBILE
+			
+			
+			xdatetime_t dt;
+			xtime_now( &dt );
+			char datetime_string[ GEMINI_DATETIME_STRING_MAX ];
+			xstr_sprintf( datetime_string, GEMINI_DATETIME_STRING_MAX, "%02d-%02d-%04d-%02d-%02d-%02d.log",
+						 dt.month, dt.day, dt.year, dt.hour, dt.minute, dt.second );
+			
+			char logdir[MAX_PATH_SIZE];
+			memset( logdir, 0, MAX_PATH_SIZE );
+			xstr_ncpy( logdir, filesystem::content_directory(), -1 );
+			xstr_cat( logdir, "/" GEMINI_LOG_PATH "/" );
+			platform::path::normalize( logdir, MAX_PATH_SIZE );
+			
+			// make sure target folder is created
+			platform::path::make_directories( logdir );
+			
+			xstr_cat( logdir, datetime_string );
+			
+			xlog_handler_t filelogger;
+			filelogger.message = file_logger_message;
+			filelogger.open = file_logger_open;
+			filelogger.close = file_logger_close;
+			filelogger.userdata = logdir;
+			
+			xlog_add_handler( &_system_log, &filelogger );
+			
+			++total_log_handlers;
+#endif
+			
+			
+#if __ANDROID__
+			xlog_handler_t android_log;
+			android_log.message = log_android_message;
+			android_log.open = log_android_open;
+			android_log.close = log_android_close;
+			android_log.userdata = 0;
+			
+			xlog_add_handler( &_system_log, &android_log );
+			++total_log_handlers;
+#endif
+			
+			if ( xlog_open( &_system_log ) < total_log_handlers )
+			{
+				fprintf( stderr, "Could not open one or more log handlers\n" );
+				error = core::Error( core::Error::Warning, "Could not open one or more log handlers" );
+			}
+			
+			LOGV( "Logging system initialized.\n" );
 			
 			return error;
 		} // open_log_handlers
@@ -55,6 +161,7 @@ namespace core
 		
 		void close_log_handlers()
 		{
+			xlog_close( &_system_log );
 		} // close_log_handlers
 	}; // namespace _internal
 	
---
+++ build/src/sdk/gemini/core/core_logging.cpp
@@ -0,0 +1,112 @@
+// -------------------------------------------------------------
+// Copyright (C) 2013- Adam Petrone
+
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM,OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+// -------------------------------------------------------------
+#include <slim/xlog.h>
+#include <slim/xstr.h> // for xstr_filefrompath
+#include <stdio.h> // for FILE, file functions
+
+#if __ANDROID__
+	#include <android/log.h>
+#endif
+
+namespace core
+{
+	// log handler function definitions
+	namespace _internal
+	{
+		void file_logger_message( xlog_handler_t * handler, const char * message, const char * filename, const char * function, int line, int type )
+		{
+			fprintf( (FILE*)handler->userdata, "[%i %s %s %i] %s", type, xstr_filefrompath(filename), function, line, message );
+			//fprintf( (FILE*)handler->userdata, "\t%s", message );
+			fflush( (FILE*)handler->userdata );
+		}
+		
+		int file_logger_open( xlog_handler_t * handler )
+		{
+			const char * logname = (const char*)handler->userdata;
+			handler->userdata = fopen( logname, "wb" );
+			return handler->userdata != 0;
+		}
+		
+		void file_logger_close( xlog_handler_t * handler )
+		{
+			if ( handler->userdata )
+			{
+				fclose( (FILE*)handler->userdata );
+			}
+		}
+		
+		
+		void stdout_message( xlog_handler_t * handler, const char * message, const char * filename, const char * function, int line, int type )
+		{
+			const char *message_types[] = { 0, "VERBOSE", "WARNING", " ERROR " };
+			fprintf( stdout, "[%s] %s, %s, %i | %s", message_types[ type ], xstr_filefrompath(filename), function, line, message );
+			//fflush( stdout );
+		}
+		
+		int stdout_open( xlog_handler_t * handler )
+		{
+			return 1;
+		}
+		
+		void stdout_close( xlog_handler_t * handler )
+		{
+		}
+
+#if _WIN32
+		void vs_message( xlog_handler_t * handler, const char * message, const char * filename, const char * function, int line, int type )
+		{
+	//		const char *message_types[] = { 0, "VERBOSE", "WARNING", " ERROR " };
+//			fprintf( stdout, "[%s] %s, %s, %i | %s", message_types[ type ], xstr_filefrompath(filename), function, line, message );
+
+
+			OutputDebugStringA( message );
+		}
+		
+		int vs_open( xlog_handler_t * handler )
+		{
+			return 1;
+		}
+		
+		void vs_close( xlog_handler_t * handler )
+		{
+		}	
+#endif
+		
+#if __ANDROID__
+		void log_android_message( xlog_handler_t * handler, const char * message, const char * filename, const char * function, int line, int type )
+		{
+			// this must match with the android_LogPriority enum in <android/log.h>
+			android_LogPriority message_types[] = { ANDROID_LOG_UNKNOWN, ANDROID_LOG_VERBOSE, ANDROID_LOG_WARN, ANDROID_LOG_ERROR };
+			__android_log_print( message_types[ type ], "gemini", message );
+		}
+		
+		int log_android_open( xlog_handler_t * handler )
+		{
+			return 1;
+		}
+		
+		void log_android_close( xlog_handler_t * handler )
+		{
+		}
+#endif
+	}; // namespace _internal
+}; // namespace core
