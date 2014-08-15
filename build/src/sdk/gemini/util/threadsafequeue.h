301264f3cdf7dd9b8eb96de47dbb6757243011bf
Adam Petrone <adampetrone83@gmail.com>
2014-08-15 09:54:02 -0400

    updated date

--- build/src/sdk/gemini/util/threadsafequeue.h
+++ build/src/sdk/gemini/util/threadsafequeue.h
@@ -1,5 +1,5 @@
 // -------------------------------------------------------------
-// Copyright (C) 2013- Adam Petrone
+// Copyright (C) 2014- Adam Petrone
 
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the "Software"),
