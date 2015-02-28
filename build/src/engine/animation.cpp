c6e3c0af7155851657ca495f533c1627efc93a9b
Adam Petrone <adampetrone83@gmail.com>
2015-02-28 12:39:27 -0500

    added missing animation files

---
+++ build/src/engine/animation.cpp
@@ -0,0 +1,335 @@
+// -------------------------------------------------------------
+// Copyright (C) 2015- Adam Petrone
+// All rights reserved.
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//		* Redistributions of source code must retain the above copyright notice,
+//		this list of conditions and the following disclaimer.
+
+//		* Redistributions in binary form must reproduce the above copyright notice,
+//		this list of conditions and the following disclaimer in the documentation
+//		and/or other materials provided with the distribution.
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//		 SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// -------------------------------------------------------------
+#include "animation.h"
+#include <platform/mem.h>
+
+#include <core/interpolation.h>
+#include <core/mathlib.h>
+#include <core/dictionary.h>
+#include <core/logging.h>
+
+#include <vector>
+
+namespace gemini
+{
+	namespace animation
+	{
+		//
+		// KeyframeList
+		//
+		
+		KeyframeList::KeyframeList() : keys(0), total_keys(0), duration_seconds(0.0f)
+		{}
+		
+		KeyframeList::~KeyframeList()
+		{
+			deallocate();
+		}
+		
+		void KeyframeList::allocate(size_t key_count)
+		{
+			total_keys = key_count;
+			keys = CREATE_ARRAY(Keyframe, total_keys);
+		}
+		
+		void KeyframeList::deallocate()
+		{
+			DESTROY_ARRAY(Keyframe, keys, total_keys);
+			total_keys = 0;
+		}
+		
+		void KeyframeList::set_key(const size_t index, const float seconds, const float value)
+		{
+			keys[index].seconds = seconds;
+			keys[index].value = value;
+		}
+	
+		//
+		// Channel
+		//
+
+		Channel::Channel(float* target, bool should_wrap) : value(target)
+		{
+			keyframelist = 0;
+			local_time_seconds = 0.0f;
+			current_keyframe = 0;
+			wrap = should_wrap;
+		}
+		
+		Channel::~Channel()
+		{}
+		
+		void Channel::set_target(float *target)
+		{
+			value = target;
+		}
+		
+		void Channel::set_keyframe_list(KeyframeList* source_keyframe_list)
+		{
+			keyframelist = source_keyframe_list;
+		}
+		
+		void Channel::advance(float delta_seconds)
+		{
+			if (!keyframelist)
+			{
+				return;
+			}
+			
+			if (!value)
+			{
+				return;
+			}
+			
+			assert(keyframelist->duration_seconds > 0.0f);
+			
+			local_time_seconds += delta_seconds;
+			
+			// determine the next keyframe; this must not wrap
+			uint32_t next_keyframe = current_keyframe + 1;
+			if (next_keyframe > keyframelist->total_keys)
+			{
+				next_keyframe = keyframelist->total_keys;
+			}
+			
+			// our local time has passed the sequence duration
+			if (local_time_seconds > keyframelist->duration_seconds)
+			{
+				if (wrap)
+				{
+					local_time_seconds -= keyframelist->duration_seconds;
+				}
+				else
+				{
+					local_time_seconds = keyframelist->duration_seconds;
+				}
+			}
+			
+			float last_keyframe_time = keyframelist->keys[current_keyframe].seconds;
+			float next_time = keyframelist->keys[next_keyframe].seconds;
+			assert(next_time != 0.0f);
+			
+			// alpha is calculated by dividing the deltas: (a/b)
+			// a. The delta between the current simulation time and the last key frame's time
+			// b. The delta between the next key frame's time and the last key frame's time.
+			float alpha = (local_time_seconds - last_keyframe_time) / (next_time - last_keyframe_time);
+			alpha = glm::clamp(alpha, 0.0f, 1.0f);
+			
+			// update value
+			float prev_value = keyframelist->keys[current_keyframe].value;
+			float next_value;
+			//		if ((current_keyframe+1) >= keyframelist->total_keys)
+			//		{
+			//			// TODO: should use post-infinity here
+			//			// For now, just use the last keyframe.
+			//			next_value = keyframelist->keys[current_keyframe].value;
+			//		}
+			//		else
+			{
+				next_value = keyframelist->keys[next_keyframe].value;
+			}
+			
+			// lerp the value
+			*value = core::lerp(prev_value, next_value, alpha);
+			
+			// determine if we should advance the keyframe index
+			if (local_time_seconds >= next_time)
+			{
+				// advance the frame
+				++current_keyframe;
+				
+				// catch out of frame bounds
+				if (current_keyframe == keyframelist->total_keys-1)
+				{
+					current_keyframe = 0;
+					local_time_seconds -= next_time;
+				}
+			}
+		} // advance
+		
+		float Channel::operator()() const
+		{
+			return *value;
+		} // operator()
+	
+	
+	
+		// AnimatedInstance
+		
+		AnimatedInstance::AnimatedInstance() :
+			local_time_seconds(0.0),
+			enabled(false)
+		{
+		}
+		
+		void AnimatedInstance::initialize(Sequence* sequence)
+		{
+			// reserve enough space
+			AnimationSet.allocate(sequence->AnimationSet.size());
+			ChannelSet.allocate(sequence->AnimationSet.size());
+			
+			// associate the channels with the keyframe lists
+			size_t index = 0;
+			for (const KeyframeListArray& array : sequence->AnimationSet)
+			{
+				AnimationSet[index].allocate(sequence->AnimationSet[index].size());
+				ChannelSet[index].allocate(sequence->AnimationSet[index].size());
+				for (size_t channel_index = 0; channel_index < sequence->AnimationSet[index].size(); ++channel_index)
+				{
+					Channel& channel = ChannelSet[index][channel_index];
+					channel.set_keyframe_list(&sequence->AnimationSet[index][channel_index]);
+					channel.set_target(&AnimationSet[index][channel_index]);
+				 }
+				
+				++index;
+			}
+		}
+		
+		void AnimatedInstance::advance(float delta_seconds)
+		{
+			for (auto& channelset : ChannelSet)
+			{
+				for (Channel& channel : channelset)
+				{
+					channel.advance(delta_seconds);
+				}
+			}
+		
+//			for (size_t index = 0; index < ChannelSet.size(); ++index)
+//			{
+//				for (size_t channel_index = 0; channel_index < ChannelSet[index].size(); ++channel_index)
+//				{
+//					Channel& channel = ChannelSet[index][channel_index];
+//					channel.advance(delta_seconds);
+//				}
+//			}
+		}
+	
+	
+	
+		//
+		// animation system stuff
+		//
+		
+		namespace detail
+		{
+			typedef core::Dictionary<Sequence*> SequenceHash;
+			typedef std::vector<Sequence*> SequenceArray;
+			typedef std::vector<AnimatedInstance*> InstanceArray;
+			
+			SequenceHash _sequences_by_name;
+			SequenceArray _sequences;
+			InstanceArray _instances;
+			
+			
+			Sequence* load_sequence_from_file(const char* name)
+			{
+				if (_sequences_by_name.has_key(name))
+				{
+					Sequence* data = 0;
+					_sequences_by_name.get(name, data);
+					return data;
+				}
+				
+				// TODO: do file loading
+				Sequence* sequence = CREATE(Sequence);
+				
+				// load the data into the sequence
+				sequence->AnimationSet.allocate(1);
+				sequence->AnimationSet[0].allocate(1);
+				KeyframeList& kfl = sequence->AnimationSet[0][0];
+				kfl.allocate(2);
+				kfl.set_key(0, 0.0f, 0.0f);
+				kfl.set_key(1, 1.0f, 100.0f);
+				
+				
+				_sequences.push_back(sequence);
+				_sequences_by_name.insert(name, sequence);
+				
+				
+				
+				return 0;
+			} // load_sequence_from_file
+		}
+		
+		
+		void startup()
+		{
+			LOGV("startup animation system: try to load a sequence...\n");
+			
+			Sequence* sequence = detail::load_sequence_from_file("animations/idle");
+		}
+		
+		void shutdown()
+		{
+			for (Sequence* sequence : detail::_sequences)
+			{
+				DESTROY(Sequence, sequence);
+			}
+			detail::_sequences.clear();
+			
+			for (AnimatedInstance* instance : detail::_instances)
+			{
+				DESTROY(AnimatedInstance, instance);
+			}
+			detail::_instances.clear();
+			
+			detail::_sequences_by_name.clear();
+		}
+		
+		
+		void update(float delta_seconds)
+		{
+			
+		}
+
+		
+		SequenceId load_sequence(const char* name)
+		{
+			return -1;
+		}
+		
+		SequenceId find_sequence(const char* name)
+		{
+			return -1;
+		}
+		
+		Sequence* get_sequence_by_index(SequenceId index)
+		{
+			return 0;
+		}
+		
+		AnimatedInstance* create_sequence_instance(SequenceId index)
+		{
+			return 0;
+		}
+		
+		void destroy_sequence_instance(AnimatedInstance* instance)
+		{
+			
+		}
+		
+	} // namespace animation
+} // namespace gemini
---
+++ build/src/engine/animation.h
@@ -0,0 +1,141 @@
+// -------------------------------------------------------------
+// Copyright (C) 2015- Adam Petrone
+// All rights reserved.
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//		* Redistributions of source code must retain the above copyright notice,
+//		this list of conditions and the following disclaimer.
+
+//		* Redistributions in binary form must reproduce the above copyright notice,
+//		this list of conditions and the following disclaimer in the documentation
+//		and/or other materials provided with the distribution.
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//		 SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// -------------------------------------------------------------
+#pragma once
+
+#include <core/typedefs.h>
+
+#include <core/fixedarray.h>
+
+namespace gemini
+{
+	namespace animation
+	{
+		struct Keyframe
+		{
+			// absolute time for the keyframe
+			float seconds;
+			
+			// value at seconds
+			float value;
+		}; // Keyframe
+	
+		struct KeyframeList
+		{
+			Keyframe* keys;
+			uint32_t total_keys;
+			float duration_seconds;
+			
+			KeyframeList();
+			~KeyframeList();
+			
+			void allocate(size_t key_count);
+			void deallocate();
+			void set_key(const size_t index, const float seconds, const float value);
+		}; // KeyframeList
+	
+		// This contains a stateful representation of a KeyframeList.
+		// It only uses the keyframe list as a source of data.
+		class Channel
+		{
+		private:
+			// source keyframe list
+			KeyframeList* keyframelist;
+			
+			// local time in seconds [0, keyframelist->duration_seconds]
+			float local_time_seconds;
+			
+			// current key frame index [0, keyframelist->total_keys-1]
+			uint32_t current_keyframe;
+			
+			// value by reference
+			float* value;
+			
+			// is this a loopable anim; if so, we ignore the last keyframe and wrap
+			// because keyframe[first] == keyframe[last]
+			bool wrap;
+			
+		public:
+			Channel(float* target = 0, bool should_wrap = true);
+			~Channel();
+			
+			void set_target(float* target);
+			void set_keyframe_list(KeyframeList* source_keyframe_list);
+			void advance(float delta_seconds);
+			
+			
+			float operator()() const;
+		}; // Channel
+	
+	
+		//
+		// supporting structures
+		//
+	
+		typedef int32_t SequenceId;
+		typedef core::FixedArray<KeyframeList> KeyframeListArray;
+		struct Sequence
+		{
+			// length of this sequence in seconds
+			float duration_seconds;
+		
+			SequenceId index;
+
+			core::FixedArray<KeyframeListArray> AnimationSet;
+		};
+	
+		
+		
+		struct AnimatedInstance
+		{
+			float local_time_seconds;
+			SequenceId sequence_index;
+			core::FixedArray< core::FixedArray<float> > AnimationSet;
+			core::FixedArray< core::FixedArray<Channel> > ChannelSet;
+			bool enabled;
+						
+			
+			AnimatedInstance();
+			virtual ~AnimatedInstance() {}
+			
+			virtual void initialize(Sequence* sequence);
+			virtual void advance(float delta_seconds);
+			virtual bool is_playing() const { return enabled; }
+		};
+	
+		//
+		// animation system
+		//
+		void startup();
+		void shutdown();
+		void update(float delta_seconds);
+
+		SequenceId load_sequence(const char* name);
+		SequenceId find_sequence(const char* name);
+		Sequence* get_sequence_by_index(SequenceId index);
+		
+		AnimatedInstance* create_sequence_instance(SequenceId index);
+		void destroy_sequence_instance(AnimatedInstance* instance);
+	}
+} // namespace gemini
