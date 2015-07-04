ae221599317618d03ffd5e4aed3813ccad9205d8
Adam Petrone <adampetrone83@gmail.com>
2015-07-04 14:30:03 -0400

    sign compare warning fix

--- build/src/tools/datamodel/skeleton.h
+++ build/src/tools/datamodel/skeleton.h
@@ -58,7 +58,7 @@ namespace gemini
 			
 			Bone* add_bone(int32_t parent_index, const String& name);
 			Bone* find_bone_named(const String& name);
-			Bone* get_bone_at_index(int32_t index);
+			Bone* get_bone_at_index(uint32_t index);
 		};
 		
 	} // namespace datamodel
