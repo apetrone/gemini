56df7978b0e07e825e95d3eb8f008f5630cdb5a0
Adam Petrone <adampetrone83@gmail.com>
2015-07-09 19:33:52 -0400

    Making the sync script a bit more robust

--- build/scripts/sync_sdks.py
+++ build/scripts/sync_sdks.py
@@ -14,23 +14,31 @@ import subprocess
 # cumbersome sdks (fbx) and some private sdks
 # which don't apply on all platforms.
 
-TARGET_LOCATION = "build/dependencies/sdks"
+DEPENDENCIES_ROOT = os.path.join("build", "dependencies")
+TARGET_LOCATION = os.path.join(DEPENDENCIES_ROOT, "sdks")
 BUILD_SERVER_HOST = "solo"
 
 if __name__ == "__main__":
 	logging.basicConfig(level=logging.INFO)
 
-	logging.info("Cloning SDKs repo from git...")
-	try:
-		command = [
-			"git",
-			"clone",
-			"git://%s/gemini/sdks.git" % BUILD_SERVER_HOST,
-			TARGET_LOCATION
-		]
+	# the dependency root folder should already exist
+	if not os.path.exists(DEPENDENCIES_ROOT):
+		raise Exception("This script must be run from the repository root!")
 
-		subprocess.call(command)
-	except:
-		raise
+	if not os.path.exists(TARGET_LOCATION):
+		logging.info("Cloning SDKs repo from git...")
+		try:
+			command = [
+				"git",
+				"clone",
+				"git://%s/gemini/sdks.git" % BUILD_SERVER_HOST,
+				TARGET_LOCATION
+			]
 
-	logging.info("Completed SDK sync.")
+			subprocess.call(command)
+		except:
+			raise
+
+		logging.info("Completed SDK sync.")
+	else:
+		logging.info("Nothing to do. '%s' exists" % TARGET_LOCATION)
