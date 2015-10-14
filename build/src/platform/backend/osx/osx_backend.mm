ee43e12367c933a09fc62ce4218397fc8017bad8
Adam Petrone <adampetrone83@gmail.com>
2015-10-14 00:09:17 -0400

    experiment with cocoa mouse acceleration and coalescing

--- build/src/platform/backend/osx/osx_backend.mm
+++ build/src/platform/backend/osx/osx_backend.mm
@@ -27,10 +27,18 @@
 
 #include <unistd.h>
 
+#include <IOKit/hid/IOHIDKeys.h>
+#include <IOKit/hidsystem/IOHIDShared.h>
+
 namespace platform
 {
 	namespace cocoa
 	{
+		// during startup, we save the old mouse acceleration so we can
+		// restore it during shutdown.
+		static double _saved_mouse_acceleration = 0.0f;
+		static io_connect_t _mouse_device = 0;
+
 		// ---------------------------------------------------------------------
 		// utilities
 		// ---------------------------------------------------------------------
@@ -52,6 +60,79 @@ namespace platform
 	// ---------------------------------------------------------------------
 	Result backend_startup()
 	{
+
+		// try to find the HIDsystem
+#if 0
+
+		mach_port_t master_port;
+		kern_return_t kern_result;
+		io_iterator_t matching_services;
+
+
+		kern_result = IOMasterPort(MACH_PORT_NULL, &master_port);
+		if (kern_result != KERN_SUCCESS)
+		{
+			return Result::failure("Unable to obtain master port");
+		}
+
+		CFMutableDictionaryRef classes_to_match = IOServiceMatching("IOHIDSystem");
+		if (!classes_to_match)
+		{
+			return Result::failure("IOServiceMatching returned a NULL dictionary");
+		}
+
+
+		// find all IOHIDSystem classes
+		kern_result = IOServiceGetMatchingServices(master_port, classes_to_match, &matching_services);
+		if (kern_result != KERN_SUCCESS)
+		{
+			// No such device was found.
+			return Result::failure(core::str::format("IOServiceGetMatchingServices returned %d\n", kern_result));
+		}
+
+		io_object_t service_interface;
+
+		if ((service_interface = IOIteratorNext(matching_services)))
+		{
+			// try to open a connection to the HIDSystem User client
+			kern_result = IOServiceOpen(service_interface, mach_task_self(), kIOHIDParamConnectType, &cocoa::_mouse_device);
+			if (kern_result != KERN_SUCCESS)
+			{
+				PLATFORM_LOG(LogMessageType::Warning, "IOServiceOpen return error 0x%X\n", kern_result);
+				IOObjectRelease(cocoa::_mouse_device);
+			}
+			else
+			{
+				// try to fetch mouse acceleration from the device
+				// if this succeeds, the device is a mouse.
+				double acceleration = 0.0;
+				kern_result = IOHIDGetMouseAcceleration(cocoa::_mouse_device, &acceleration);
+				if (kern_result != KERN_SUCCESS)
+				{
+					// this is not a mouse device.
+					IOServiceClose(cocoa::_mouse_device);
+					IOObjectRelease(cocoa::_mouse_device);
+				}
+				else
+				{
+					// we have found a mouse
+					PLATFORM_LOG(LogMessageType::Info, "Found mouse; acceleration is: %2.2f\n", acceleration);
+
+					cocoa::_saved_mouse_acceleration = acceleration;
+
+					acceleration = 0.0f;
+					IOHIDSetMouseAcceleration(cocoa::_mouse_device, acceleration);
+
+					IOHIDGetMouseAcceleration(cocoa::_mouse_device, &acceleration);
+					PLATFORM_LOG(LogMessageType::Info, "Reset Mouse Acceleration to %2.2f\n", acceleration);
+				}
+			}
+		}
+
+
+		// disable mouse coalescing
+		[NSEvent setMouseCoalescingEnabled:NO];
+#endif
 //
 //		NSProcessInfo* processInfo = [NSProcessInfo processInfo];
 //
@@ -75,6 +156,10 @@ namespace platform
 
 	void backend_shutdown()
 	{
+		if (cocoa::_mouse_device != 0)
+		{
+			IOHIDSetMouseAcceleration(cocoa::_mouse_device, cocoa::_saved_mouse_acceleration);
+		}
 	}
 
 	void backend_log(LogMessageType, const char* message)
