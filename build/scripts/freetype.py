f41430f1e2996bb1e061c7307591bd90dae40d12
Adam Petrone <adampetrone83@gmail.com>
2015-11-03 21:24:39 -0500

    finally added freetype2 library; fix up build script

--- build/scripts/freetype.py
+++ build/scripts/freetype.py
@@ -6,44 +6,47 @@ def products(arguments, **kwargs):
 	target_platform = kwargs.get("target_platform")
 
 	freetype = Product(name="freetype", output=ProductType.DynamicLibrary)
-	freetype.root = "../dependencies/freetype-2.4.2"
+	freetype.root = "../dependencies/freetype2"
 	freetype.project_root = "_projects"
-	freetype.sources += [
+	freetype.sources += [	
 		"src/autofit/autofit.c",
-		"src/bdf/bdf.c",
-		"src/cff/cff.c",
 		"src/base/ftbase.c",
+		"src/base/ftbbox.c",
+		"src/base/ftbdf.c",
 		"src/base/ftbitmap.c",
-		"src/cache/ftcache.c",
+		"src/base/ftcid.c",
+		"src/base/ftfntfmt.c",		
 		"src/base/ftfstype.c",
 		"src/base/ftgasp.c",
 		"src/base/ftglyph.c",
-		"src/gzip/ftgzip.c",
-		"src/base/ftinit.c",
-		"src/lzw/ftlzw.c",
-		"src/base/ftstroke.c",
-		"src/base/ftsystem.c",
-		"src/smooth/smooth.c",
-		"src/base/ftbbox.c",
-		"src/base/ftmm.c",
-		"src/base/ftpfr.c",
-		"src/base/ftsynth.c",
-		"src/base/fttype1.c",
-		"src/base/ftlcdfil.c",
 		"src/base/ftgxval.c",
+		"src/base/ftinit.c",
+		"src/base/ftlcdfil.c",
+		"src/base/ftmm.c",
 		"src/base/ftotval.c",
 		"src/base/ftpatent.c",
-		"src/base/ftxf86.c",
+		"src/base/ftpfr.c",
+		"src/base/ftstroke.c",
+		"src/base/ftsynth.c",
+		"src/base/ftsystem.c",
+		"src/base/fttype1.c",
+		"src/bdf/bdf.c",
+		"src/bzip2/ftbzip2.c",
+		"src/cache/ftcache.c",
+		"src/cff/cff.c",
+		"src/cid/type1cid.c",
+		"src/gzip/ftgzip.c",
+		"src/lzw/ftlzw.c",
 		"src/pcf/pcf.c",
 		"src/pfr/pfr.c",
 		"src/psaux/psaux.c",
 		"src/pshinter/pshinter.c",
-		"src/psnames/psmodule.c",
+		"src/psnames/psnames.c",
 		"src/raster/raster.c",
 		"src/sfnt/sfnt.c",
+		"src/smooth/smooth.c",
 		"src/truetype/truetype.c",
 		"src/type1/type1.c",
-		"src/cid/type1cid.c",
 		"src/type42/type42.c",
 		"src/winfonts/winfnt.c",
 
@@ -83,6 +86,12 @@ def products(arguments, **kwargs):
 	macosx.includes += [
 		"builds/ansi",
 		"objs"
+	]
+	macosx.links += [
+		"Carbon.framework",
+		"CoreServices.framework",
+		"CoreFoundation.framework",
+		"CoreText.framework"
 	]
 	macosx.defines += [
 		"FT_MACINTOSH=1",
