2e7907adc2908fa8976aa0d859b73caeda57bb38
Adam Petrone <adampetrone83@gmail.com>
2016-11-20 01:29:04 +0000

    Additional debug info when detecting X11 or bcm_host

--- build/scripts/gemini.py
+++ build/scripts/gemini.py
@@ -1000,6 +1000,7 @@ def products(arguments, **kwargs):
 		# Is this a RaspberryPi?
 		bcm_host_h = target_platform.find_include_path("bcm_host.h")
 		if bcm_host_h:
+			logging.info('Detected bcm_host.h; identifying machine as RaspberryPi...')
 			arguments.raspberrypi = True
 
 		# Only prefer X11 if not on RaspberryPi.
@@ -1008,6 +1009,7 @@ def products(arguments, **kwargs):
 			found_xlib = target_platform.find_include_path("X11/Xlib.h")
 			if found_xlib:
 				arguments.with_x11 = True
+				logging.info('Detected Xlib.h; building with X11...')
 
 	if arguments.raspberrypi:
 		if not arguments.gles:
