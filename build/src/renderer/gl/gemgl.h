72fa91895be3bc54c25a20bbee61abf5c5ba77ab
Adam Petrone <adampetrone83@gmail.com>
2017-01-28 22:17:13 -0500

    Fix subtle, but major issue with glClearDepth function declaration.
    - This was causing the wrong value to be passed to gl.ClearDepth -- resulting in the
      depth test failing miserably.

--- build/src/renderer/gl/gemgl.h
+++ build/src/renderer/gl/gemgl.h
@@ -112,7 +112,7 @@ namespace renderer
 		typedef void (GEMGLAPI GEMGLFNDRAWARRAYS) ( GLenum mode, GLint first, GLsizei count );
 		typedef void (GEMGLAPI GEMGLFNDRAWELEMENTS) ( GLenum mode, GLsizei count, GLenum type, GLvoid* indices );
 		typedef void (GEMGLAPI GEMGLFNCLEARSTENCIL) (GLint stencil);
-		typedef void (GEMGLAPI GEMGLFNCLEARDEPTH) (GLclampf depth);
+		typedef void (GEMGLAPI GEMGLFNCLEARDEPTH) (GLclampd depth);
 		typedef void (GEMGLAPI GEMGLFNCLEARCOLOR) ( GLclampf red, GLclampf green, GLclampf blue, GLclampf alpha );
 		typedef void (GEMGLAPI GEMGLFNCLEAR) ( GLbitfield mask );
 		typedef void (GEMGLAPI GEMGLFNPOINTSIZE) ( GLfloat size );
